24ac86bdb6ca09437f71f6274cade93fb762fb60
Michal <preterer@gmail.com>
2019-12-06 21:07:36 +0100

    Updated missing docs

--- src/views/home/Home.ts
+++ src/views/home/Home.ts
@@ -4,5 +4,12 @@ import Component from "vue-class-component";
 import { Login } from "../../components/login/Login";
 import template from "./Home.html";
 
+/**
+ * Home component
+ *
+ * @export
+ * @class Home
+ * @extends {Vue}
+ */
 @Component({ name: "Home", template, components: { Login } })
 export class Home extends Vue {}
