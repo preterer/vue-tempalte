62962030b9676fb891d8b7a360c4b1aa8849020d
Michal <preterer@gmail.com>
2019-12-06 20:36:12 +0100

    Vinput

---
+++ src/components/validation/vinput/VInput.ts
@@ -0,0 +1,64 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { Validation } from "vuelidate";
+
+import { VErrors } from "../verrors/VErrors";
+import template from "./VInput.html";
+
+@Component({
+  name: "VInput",
+  template,
+  inheritAttrs: false,
+  components: { VErrors }
+})
+export class VInput extends Vue {
+  /**
+   * Data model
+   *
+   * @type {*}
+   * @memberof VInput
+   */
+  @Prop({ required: true, type: Object })
+  public model!: Validation;
+
+  /**
+   * Custom validation messages
+   *
+   * @type {{ [key: string]: string }}
+   * @memberof VInput
+   */
+  @Prop({ required: false, type: Object })
+  public messages!: { [key: string]: string };
+
+  /**
+   * Is field touched
+   *
+   * @type {boolean}
+   * @memberof VInput
+   */
+  public touched: boolean = false;
+
+  /**
+   * Error class getter
+   *
+   * @readonly
+   * @type {(string | undefined)}
+   * @memberof VInput
+   */
+  public get errorClass(): string | undefined {
+    if (this.model.$invalid && (this.touched || this.model.$dirty)) {
+      return "error";
+    }
+    return;
+  }
+
+  /**
+   * Marks field as touched
+   *
+   * @memberof VInput
+   */
+  public touch() {
+    this.touched = true;
+  }
+}
