03b5c0ac9f854d8811f1b1839349a71f48cb8389
Michal <preterer@gmail.com>
2019-12-06 21:01:05 +0100

    Missing docs

--- src/components/routeContainer/RouteContainer.ts
+++ src/components/routeContainer/RouteContainer.ts
@@ -1,5 +1,12 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+/**
+ * Renders route view
+ *
+ * @export
+ * @class RouteContainer
+ * @extends {Vue}
+ */
 @Component({ template: "<router-view />" })
 export class RouteContainer extends Vue {}
