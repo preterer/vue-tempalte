75908fb363ffba69d9c81bb607a4ae1020067496
Michal <preterer@gmail.com>
2019-12-08 22:23:04 +0100

    i18n

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import Vue from "vue";
+import VueI18n from "vue-i18n";
 import VueNotification from "vue-notification";
 import VueRouter from "vue-router";
 
 import { App } from "./App";
 import { Btn } from "./components/btn/Btn";
 import { router } from "./router/router";
+import { messages } from "./translations/messages";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueI18n);
 Vue.use(VueRouter);
 Vue.use(VueNotification);
 Vue.component("btn", Btn);
 
+const i18n = new VueI18n({ locale: "en", messages });
+
 new Vue({
   render: h => h(App),
+  i18n,
   router
 }).$mount("#app");
---
+++ src/translations/messages.ts
@@ -0,0 +1,6 @@
+import { LocaleMessages } from "vue-i18n";
+
+import { en } from "./en";
+import { pl } from "./pl";
+
+export const messages: LocaleMessages = { en, pl };
