5547bc308b7ee2c5b461b4f1b13a6c186b0f1220
Michal <preterer@gmail.com>
2019-12-08 23:14:49 +0100

    docs

--- src/components/languagePicker/LanguagePicker.ts
+++ src/components/languagePicker/LanguagePicker.ts
@@ -5,30 +5,69 @@ import { Locale } from "#/interfaces/locale";
 import { config } from "../../config/config";
 import template from "./LanguagePicker.html";
 
+/**
+ * Language picker component
+ *
+ * @export
+ * @class LanguagePicker
+ * @extends {Vue}
+ */
 @Component({ name: "LanguagePicker", template })
 export class LanguagePicker extends Vue {
-  public showMenu: boolean = false;
+  /**
+   * Is menu visible
+   *
+   * @type {boolean}
+   * @memberof LanguagePicker
+   */
+  public isMenuVisible: boolean = false;
 
+  /**
+   * All application locales list
+   *
+   * @private
+   * @type {Locale[]}
+   * @memberof LanguagePicker
+   */
   private allLocales: Locale[] = [
     { code: "en", title: "English", svgPath: "/img/en.svg" },
     { code: "pl", title: "Polski", svgPath: "/img/pl.svg" }
   ];
 
+  /**
+   * Currently selected locale
+   *
+   * @readonly
+   * @type {(Locale | undefined)}
+   * @memberof LanguagePicker
+   */
   public get selectedLocale(): Locale | undefined {
     return this.allLocales.find(locale => locale.code === this.$i18n.locale);
   }
 
+  /**
+   * Locales available to choose
+   *
+   * @readonly
+   * @type {Locale[]}
+   * @memberof LanguagePicker
+   */
   public get availableLocales(): Locale[] {
     return this.allLocales.filter(locale => locale.code !== this.$i18n.locale);
   }
 
+  /**
+   * Toggles menu display
+   *
+   * @memberof LanguagePicker
+   */
   public toggleMenu() {
-    this.showMenu = !this.showMenu;
+    this.isMenuVisible = !this.isMenuVisible;
   }
 
   public selectLocale(locale: Locale) {
     this.$root.$i18n.locale = locale.code;
     localStorage.setItem(config.localeStorageKey, locale.code);
-    this.showMenu = false;
+    this.isMenuVisible = false;
   }
 }
