165128405f74debcb2d18197abbdb797a28aa376
Michal Baranowski <preterer@gmail.com>
2020-01-14 22:05:23 +0100

    Navigation component

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1,12 +1,14 @@
 import { LocaleMessageObject } from "vue-i18n";
 
 export const en: LocaleMessageObject = {
+  "app.name": "Template",
   "errors.validation_error": "Invalid form data.",
   "Incorrect login or password.": "Incorrect login or password.",
   "home.title": "Hello!",
   "login.button": "Sign in",
   "login.title": "Sign in",
   "login.label": "Login",
+  logout: "Sign out",
   "password.label": "Password",
   "validation.required": "Field is required."
 };
--- src/translations/pl.ts
+++ src/translations/pl.ts
@@ -1,12 +1,14 @@
 import { LocaleMessageObject } from "vue-i18n";
 
 export const pl: LocaleMessageObject = {
+  "app.name": "Template",
   "errors.validation_error": "Nieprawidłowe dane formularza.",
   "Incorrect login or password.": "Niepoprawny login lub hasło.",
   "home.title": "Witaj!",
   "login.button": "Zaloguj się",
   "login.title": "Logowanie",
   "login.label": "Login",
+  logout: "Wyloguj się",
   "password.label": "Hasło",
   "validation.required": "Pole jest wymagane."
 };
