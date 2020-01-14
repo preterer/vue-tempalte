2b55facdaae951f28c2933eda3aedf2ec2636f9a
Michal Baranowski <preterer@gmail.com>
2020-01-14 22:19:55 +0100

    Navigation implementation

--- src/App.ts
+++ src/App.ts
@@ -3,10 +3,19 @@ import Component from "vue-class-component";
 
 import template from "./App.html";
 import { LanguagePicker } from "./components/languagePicker/LanguagePicker";
+import { Navigation } from "./components/navigation/Navigation";
+import { authService } from "./services/auth.service";
 
 @Component({
   name: "app",
   template: template,
-  components: { LanguagePicker }
+  components: { LanguagePicker, Navigation }
 })
-export class App extends Vue {}
+export class App extends Vue {
+  /**
+   * Auth service
+   *
+   * @memberof App
+   */
+  public authService = authService;
+}
